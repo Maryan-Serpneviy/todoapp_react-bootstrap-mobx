aeba71b6ee16555f8265664d963dc69a9db8d6d4
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2020-01-28 12:35:09 +0200

    Build

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Provider } from 'mobx-react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { HashRouter as Router, Route } from 'react-router-dom'
 import Courses from '~cn/Courses'
 import Students from '~cn/Students'
 import store from '~s'
